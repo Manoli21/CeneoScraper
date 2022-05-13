485d858daade9d05c904f72d154657110d24dd3c
Manoli21 <s222724@student.uek.krakow.pl>
2022-05-13 11:18:36 +0200

    2022-05-13 222724

--- analyzer.py
+++ analyzer.py
@@ -1,11 +1,38 @@
 import os
 import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
 
 print(*[filename.split(".")[0] for filename in os.listdir("./opinions")], sep="\n")
 product_id = input("Podaj identyfikator produktu: ")
 
 opinions = pd.read_json(f"opinions/{product_id}.json")
-print(opinions)
+# print(opinions)
+opinions.stars = opinions.stars.map(lambda x: float(x.split("/")[0].replace(",",".")))
+opinions_count = len(opinions.index)
+# opinions_count = opinions.shape[0]
+pros_count = opinions.pros.map(bool).sum()
+cons_count = opinions.pros.map(bool).sum()
+average_score = opinions.stars.mean().round(2)
 
-## Przejrzyć dokumenty pandas, policzyć ile mamy opinii, dla ile wady/zalety, liczba gwiazdek 
-## przeczytać bibliotekę pandas, matplotlib
+recommendation = opinions.recommendation.value_counts(dropna = False).sort_index().reindex(["Nie polecam","Polecam", None])
+recommendation.plot.pie(
+    label="",
+    autopct="%1.1f%%", 
+    colors=["crimson", "forestgreen", "lightskyblue"],
+    labels=["Nice polecam", "Polecam", "Nie mam zdania"]
+)
+plt.title("Rekomendacja")
+plt.savefig(f"plots/{product_id}_recommendations.png")
+plt.close()
+
+stars = opinions.stars.value_counts().sort_index().reindex(list(np.arange(0,5.5,0.5)), fill_value=0)
+stars.plot.bar()
+plt.title("Oceny produktu")
+plt.xlabel("Liczba gwiazdek")
+plt.ylabel("Liczba opinii")
+plt.grid(True)
+plt.xticks(rotation=0)
+plt.savefig(f"plots/{product_id}stars.png")
+plt.show()
+plt.close()
